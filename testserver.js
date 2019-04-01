1b591b2060c17f56de08ae4ef9d35848fe893299
Daniel Fenz <dani.fenz@aon.at>
2019-04-01 23:11:50 +0200

    Init commit

---
+++ testserver.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('static'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/sketch.html');
+});
+
+app.listen(8080, () => console.log('Dev-Server running on 8080'));
