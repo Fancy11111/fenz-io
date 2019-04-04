dccc890e55d45bf5ccc6bb2e976d58c4b4e574fe
Daniel Fenz <dani.fenz@aon.at>
2019-04-04 20:03:07 +0200

    fixed additional caching errors

--- static/sw.js
+++ static/sw.js
@@ -1,4 +1,4 @@
-importScripts('js/cache-polyfill.js');
+importScripts('/js/cache-polyfill.js');
 var CACHE_NAME = 'fenzio';
 var CACHING = [
   '/',
@@ -9,27 +9,38 @@ var CACHING = [
   '/css/brands.min.css',
   '/css/fontawesome.min.css',
   '/images/temp-background.jpg',
-  '/images/temp-background-mobile.jpg',
+  //'/images/temp-background-mobile.jpg',
   '/icons/favicon.ico',
+  //'/js/cache-polyfill.js',
   // '/open-sans-v16-latin_latin-ext700.eot',
   // '/open-sans-v16-latin_latin-ext700.svg',
   // '/open-sans-v16-latin_latin-ext700.ttf',
   // '/open-sans-v16-latin_latin-ext700.woff',
-  '/open-sans-v16-latin_latin-ext700.woff2',
+  '/fonts/open-sans-v16-latin_latin-ext-700.woff2',
   // '/open-sans-v16-latin_latin-ext-regular.eot',
   // '/open-sans-v16-latin_latin-ext-regular.svg',
   // '/open-sans-v16-latin_latin-ext-regular.ttf',
   // '/open-sans-v16-latin_latin-ext-regular.woff',
-  '/open-sans-v16-latin_latin-ext-regular.woff2',
+  '/fonts/open-sans-v16-latin_latin-ext-regular.woff2',
   '/webfonts/fa-brands-400.woff2',
   '/webfonts/fa-solid-900.woff2'
 ];
 
 self.addEventListener('install', function(e) {
   e.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll(CACHING);
-    })
+    caches
+      .open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(CACHING);
+      })
+      .catch(function(error) {
+        console.log('test');
+        let err = error || {};
+        console.error(err);
+        Object.keys(err).forEach(function(key) {
+          console.log(key);
+        });
+      })
   );
 });
 
