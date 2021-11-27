76a4aab1cc327870baa2ef30fb4709cc43dc4d97
Daniel Fenz <dani.fenz@aon.at>
2021-11-27 14:44:58 +0100

    updated configs

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-const basePath = process.env.NODE_ENV == 'production' ? '/fenz-io' : ''
+const basePath = process.env.NODE_ENV == 'production' ? '' : ''
 
 module.exports = {
   reactStrictMode: true,
